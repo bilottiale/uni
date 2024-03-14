d149c08a5ea1df9af2a191a4d088f84743cf3cf2
Alessandro <62628310+bilottiale@users.noreply.github.com>
2024-03-14 10:04:57 +0100

    Updates

---
+++ Secondo Semetre/ProgII/Lab/11-03/main.c
@@ -0,0 +1,14 @@
+/∗∗ @brief Controlla se due contatti hanno lo stesso nome e cognome
+∗/
+_Bool contactEq(Contact c1, Contact c2);
+/∗∗ @brief Controlla se due contatti hanno lo stesso nome e cognome
+∗/
+_Bool contactEqEff(const Contact const ∗pc1, const Contact const ∗pc2);
+/∗∗ @brief Confronta due contatti per cognome rispetto all’ordine lessicografico e, se il cognome e’ lo stesso, per nome
+@return −1 se c1 minore di c2, 0 se c1 uguale a c2, 1 se c1 maggiore di c2
+∗/
+int contactCmp(Contact c1, Contact c2);
+/∗∗ @brief Confronta due contatti per cognome rispetto all’ordine lessicografico e, se il cognome e’ lo stesso, per nome
+@return −1 se c1 minore di c2, 0 se c1 uguale a c2, 1 se c1 maggiore di c2
+∗/
+int contactCmpEff(const Contact const ∗pc1, const Contact const ∗pc2);
